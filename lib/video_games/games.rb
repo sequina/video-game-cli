b2aa148b0c882458f18aa189c6ca4907b883a961
Sequina Hall <sccaro89@gmail.com>
2017-05-22 11:34:33 -0500

    receiving data from scrape

--- lib/video_games/games.rb
+++ lib/video_games/games.rb
@@ -17,11 +17,15 @@ class VideoGames::Games
   end
 
   def self.scrape_games
-    doc = Nokogiri::HTML(open("https://www.groupon.com/browse/nashville?lat=36.16589&lng=-86.78444&administrative_area=TN&locality=Nashville&address=Nashville%2C+TN&query=video+games&locale=en_US"))
+    doc = Nokogiri::HTML(open("http://www.gameinformer.com/b/news/archive/2016/10/10/2017-video-game-release-schedule.aspx"))
 
-     doc.css('.cui-udc-details').each do |game|
-       puts game.content
-     end
+    # within the span element this takes the first parent, wich is the p tag and then goes into the span tag and pulls out the month
+    months = doc.search("div#divRenderBody p strong span")
+
+    #this pulls out the list of games inside the span element
+    the_containing_p = months.first.parent.parent
+    #pulls the list just for january
+    the_list_of_jan = the_containing_p.text
      binding.pry
       #  game.name
   end
