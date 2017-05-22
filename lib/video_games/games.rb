ca78a6251374b630a38a3be573509293dfb62b81
Sequina Hall <sccaro89@gmail.com>
2017-05-21 20:14:14 -0500

    scrape for games is working

--- lib/video_games/games.rb
+++ lib/video_games/games.rb
@@ -1,5 +1,5 @@
 class VideoGames::Games
-  attr_accessor :name, :date, :console
+  attr_accessor :name, :price
 
   def self.list_games
     #should return list of games from site and its data
@@ -17,8 +17,13 @@ class VideoGames::Games
   end
 
   def self.scrape_games
-    doc = Nokogiri::HTML(open("http://www.gameinformer.com/b/news/archive/2016/10/10/2017-video-game-release-schedule.aspx"))
+    doc = Nokogiri::HTML(open("https://www.groupon.com/browse/nashville?lat=36.16589&lng=-86.78444&administrative_area=TN&locality=Nashville&address=Nashville%2C+TN&query=video+games&locale=en_US"))
+
+     doc.css('.cui-udc-details').each do |game|
+       puts game.content
+     end
      binding.pry
+      #  game.name
   end
 
 
